0572fd9f0a139dd90e4226dc42ec68d20b25630c
Geraldo P Melo <gpatricio.melo@gmail.com>
2026-05-18 12:55:40 -0300

    Add ProdutosView and ClientesView routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import PDVView from '../views/PDVView.vue'
 import ECommerceMobile from '@/views/ECommerceMobile.vue'
+import ProdutosView from '../views/ProdutosView.vue'
+import ClientesView from '../views/ClientesView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +22,16 @@ const router = createRouter({
       path: '/ecommerce',
       name: 'ecommerce',
       component: ECommerceMobile
+    },
+    {
+      path: '/produtos',
+      name: 'produtos',
+      component: ProdutosView
+    },
+    {
+      path: '/clientes',
+      name: 'clientes',
+      component: ClientesView
     }
   ]
 })
